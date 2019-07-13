c101d3cae5559c7e5bdb90204c13aa578f74d09f
Risul Karim <risulkarimx1@gmail.com>
2019-07-13 16:44:27 +0200

    [2] Added Grid Class

---
+++ PoissonDiscSampling/Assets/Script/Grid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+[Serializable]
+public class Grid
+{
+    public int Width;
+    public int Height;
+
+    public Vector2[] Values;
+
+    public void CreateGrid(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Values = new Vector2[width * height];
+    }
+
+    public void AddValue(int col, int row, Vector2 Value)
+    {
+        var index = GetIndex(col, row);
+        Values[index] = Value;
+    }
+    private int GetIndex(int col, int row)
+    {
+        return col + row * Width;
+    }
+    public Vector2 GetValue(int col, int row)
+    {
+
+        return Values[GetIndex(col, row)];
+    }
+    public Vector2 GetValue(float col, float row)
+    {
+        return GetValue((int)col, (int)row);
+    }
+}
